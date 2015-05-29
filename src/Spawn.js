b75f15ee14f150b7c22f6c10fc420864cb55e7d2
Estel Smith <estel.smith@gmail.com>
2015-05-29 18:22:14 -0400

    Added the Spawn object.

---
+++ src/Spawn.js
@@ -0,0 +1,90 @@
+var Spawn = function () {};
+
+Spawn.prototype = {
+    /**
+     * @type number
+     */
+    energy: 0,
+
+    /**
+     * @type number
+     */
+    energyCapacity: 0,
+
+    /**
+     * @type number
+     */
+    hits: 0,
+
+    /**
+     * @type number
+     */
+    hitsMax: 0,
+
+    /**
+     * @type string
+     */
+    id: '',
+
+    /**
+     * @type object
+     */
+    memory: {},
+
+    /**
+     * @type boolean
+     */
+    my: false,
+
+    /**
+     * @type string
+     */
+    name: '',
+
+    /**
+     * @type object
+     */
+    owner: {},
+
+    /**
+     * @type RoomPosition
+     */
+    pos: null,
+
+    /**
+     * @type Room
+     */
+    room: null,
+
+    /**
+     * @type string
+     */
+    structureType: '',
+
+    /**
+     * @type object|null
+     */
+    spawning: null,
+
+    /**
+     * @param {Array<WORK|MOVE|CARRY|ATTACK|RANGED_ATTACK|HEAL|TOUGH>} body
+     * @param {string} name
+     * @return {OK|ERR_NOT_OWNER|ERR_NAME_EXISTS|ERR_BUSY|ERR_NOT_ENOUGH_ENERGY|ERR_INVALID_ARGS}
+     */
+    canCreateCreep: function (body, name) {},
+
+    /**
+     * @param {Array<WORK|MOVE|CARRY|ATTACK|RANGED_ATTACK|HEAL|TOUGH>} body
+     * @param {string} name
+     * @param {object} memory
+     * @return {string|ERR_NOT_OWNER|ERR_NAME_EXISTS|ERR_BUSY|ERR_NOT_ENOUGH_ENERGY|ERR_INVALID_ARGS}
+     */
+    createCreep: function (body, name, memory) {},
+
+    /**
+     * @param {Creep} target
+     * @param {number} amount
+     * @return {OK|ERR_NOT_OWNER|ERR_NOT_ENOUGH_ENERGY|ERR_INVALID_TARGET|ERR_FULL|ERR_NOT_IN_RANGE}
+     */
+    transferEnergy: function (target, amount) {}
+};
