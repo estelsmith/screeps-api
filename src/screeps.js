60eae7082fe74c7d4a40f4d47d4db3886366cb2f
Estel Smith <estel.smith@gmail.com>
2015-05-29 16:35:27 -0400

    Added Room.find constants.

--- src/screeps.js
+++ src/screeps.js
@@ -1,3 +1,8 @@
+/*
+ *************************
+ * Result Code Constants *
+ *************************
+ */
 var
     OK = 0,
     ERR_NOT_OWNER = -1,
@@ -15,4 +20,30 @@ var
     ERR_NOT_ENOUGH_EXTENSIONS = -13,
     ERR_RCL_NOT_ENOUGH = -14,
     ERR_GCL_NOT_ENOUGH = -15
+;
+
+/*
+ ***********************
+ * Room.find Constants *
+ ***********************
+ */
+var
+    FIND_EXIT_TOP = 1,
+    FIND_EXIT_RIGHT = 3,
+    FIND_EXIT_BOTTOM = 5,
+    FIND_EXIT_LEFT = 7,
+    FIND_EXIT = 10,
+    FIND_CREEPS = 101,
+    FIND_MY_CREEPS = 102,
+    FIND_HOSTILE_CREEPS = 103,
+    FIND_SOURCES_ACTIVE = 104,
+    FIND_SOURCES = 105,
+    FIND_DROPPED_ENERGY = 106,
+    FIND_STRUCTURES = 107,
+    FIND_MY_STRUCTURES = 108,
+    FIND_HOSTILE_STRUCTURES = 109,
+    FIND_FLAGS = 110,
+    FIND_CONSTRUCTION_SITES = 111,
+    FIND_MY_SPAWNS = 112,
+    FIND_HOSTILE_SPAWNS = 113
 ;
