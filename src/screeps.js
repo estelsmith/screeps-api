250f1422de3648de60b56f223fdffc7d0c0ff888
Estel Smith <estel.smith@gmail.com>
2015-05-29 16:31:59 -0400

    Added result code constants.

---
+++ src/screeps.js
@@ -0,0 +1,18 @@
+var
+    OK = 0,
+    ERR_NOT_OWNER = -1,
+    ERR_NO_PATH = -2,
+    ERR_NAME_EXISTS = -3,
+    ERR_BUSY = -4,
+    ERR_NOT_FOUND = -5,
+    ERR_NOT_ENOUGH_ENERGY = -6,
+    ERR_INVALID_TARGET = -7,
+    ERR_FULL = -8,
+    ERR_NOT_IN_RANGE = -9,
+    ERR_INVALID_ARGS = -10,
+    ERR_TIRED = -11,
+    ERR_NO_BODYPART = -12,
+    ERR_NOT_ENOUGH_EXTENSIONS = -13,
+    ERR_RCL_NOT_ENOUGH = -14,
+    ERR_GCL_NOT_ENOUGH = -15
+;
