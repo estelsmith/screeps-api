b432d4c87efb798b8f98371a78156bda9a12b111
Estel Smith <estel.smith@gmail.com>
2015-05-29 17:30:22 -0400

    Added the Energy object.

---
+++ src/Energy.js
@@ -0,0 +1,23 @@
+var Energy = function () {};
+
+Energy.prototype = {
+    /**
+     * @type number
+     */
+    energy: 0,
+
+    /**
+     * @type string
+     */
+    id: '',
+
+    /**
+     * @type RoomPosition
+     */
+    pos: null,
+
+    /**
+     * @type Room
+     */
+    room: null
+};
